eb83c99f5f96c58141ba05df5127196bf16e8078
Chethana_Chinnaswamy <150918124+Chethana-C26@users.noreply.github.com>
2023-11-15 18:36:59 +0530

    Add files via upload

---
+++ date.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html long="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpart" content="width=device-width,initial_scale=1.0">
+        <tittle>PHP</tittle>
+    </head>
+    <body>
+        <?php
+
+        function sum() {
+            echo "Inside function";
+            $n1=3;
+            $n2=5;
+            function sum($num2 +$num1){
+                $add=$num1+$num2;
+                echo $add."<br>";
+
+            }
+            sum($n1,$n2);
+        }
+        ?>
+
+    </body>
+</html>
